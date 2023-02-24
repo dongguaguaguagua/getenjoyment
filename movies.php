af7fa96c073b00c65e80e48d8b42ce308e0317a4
dongguaguaguagua <1448424184@qq.com>
2023-02-24 13:02:48 +0800

    add headline

--- movies.php
+++ movies.php
@@ -28,65 +28,93 @@ td.link{
 
 
 <body>
-    <center>
-    <h2>欢迎来到scu资源共享站</h2>
+    <!-- Always shows a header, even in smaller screens. -->
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <!-- Title -->
+          <span class="mdl-layout-title">欢迎来到scu资源共享站</span>
+          <!-- Add spacer, to align navigation to the right -->
+          <div class="mdl-layout-spacer"></div>
+          <!-- Navigation. We hide it in small screens. -->
+          <nav class="mdl-navigation mdl-layout--large-screen-only">
+            <a class="mdl-navigation__link" href="http://localhost:8080/resources/movies.php">电影</a>
+            <a class="mdl-navigation__link" href="http://localhost:8080/resources/serial.php">电视剧</a>
+            <a class="mdl-navigation__link" href="http://localhost:8080/resources/variety_show.php">综艺</a>
+            <a class="mdl-navigation__link" href="http://localhost:8080/resources/books.php">书籍</a>
+          </nav>
+        </div>
+      </header>
+      <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title">scu资源共享站</span>
+        <nav class="mdl-navigation">
+          <a class="mdl-navigation__link" href="http://localhost:8080/resources/movies.php">电影</a>
+          <a class="mdl-navigation__link" href="http://localhost:8080/resources/serial.php">电视剧</a>
+          <a class="mdl-navigation__link" href="http://localhost:8080/resources/variety_show.php">综艺</a>
+          <a class="mdl-navigation__link" href="http://localhost:8080/resources/books.php">书籍</a>
+        </nav>
+      </div>
+      <main class="mdl-layout__content">
+        <div class="page-content">
+            <center>
+                <h3 style="color:rgb(65, 147, 136)">电影</h3>
+                <!-- 表格内容 -->
+                <form action="books.php?act=insert" method="post">
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                         <input class="mdl-textfield__input" type="text" name="name" id="name">
+                         <label class="mdl-textfield__label" for="name">你的名字……</label>
+                    </div>
+                    <br>
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                         <input class="mdl-textfield__input" type="text" name="bookName" required="required" id="bookName">
+                         <!-- <label class="mdl-textfield__label" for="bookName">电影名称(必填)</label> -->
+                    </div>
+                    <br>
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                         <input class="mdl-textfield__input" type="text" name="link" pattern="[A-Z,a-z,0-9]*" id="link">
+                         <label class="mdl-textfield__label" for="link">资源链接(必须是永久链接)</label>
+                         <span class="mdl-textfield__error">链接只能包含字母和数字</span>
+                    </div>
+                    <br>
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                         <input class="mdl-textfield__input" type="text" name="extractCode" id="extractCode">
+                         <label class="mdl-textfield__label" for="extractCode">提取码（如果有的话）</label>
+                    </div>
+                    <br>
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                         <textarea class="mdl-textfield__input" type="text" name="notes" id="notes" ></textarea>
+                         <label class="mdl-textfield__label" for="notes">备注……</label>
+                    </div>
+                    <br>
+                    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored" type="submit">
+                        分享资源
+                    </button>
+                 </form>
+                 <br>
+                 <!-- <div class="mdl-spinner mdl-js-spinner is-active"></div> -->
 
-    <a href="http://localhost:8080/resources">首页</a>
-    <a href="http://localhost:8080/resources/movies.php">电影</a>
-    <a href="http://localhost:8080/resources/serial.php">电视剧</a>
-    <a href="http://localhost:8080/resources/variety_show.php">综艺</a>
-    <a href="http://localhost:8080/resources/books.php">书籍</a>
-
-    <form action="books.php?act=insert" method="post">
-        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-             <input class="mdl-textfield__input" type="text" name="name" id="name">
-             <label class="mdl-textfield__label" for="name">你的名字……</label>
+                <!-- 加载中 -->
+                 <div id="load"><span class="mdl-chip"><span class="mdl-chip__text">加载中……</span></span></div>
+                 <br>
+                <!-- 搜索框 -->
+                <div class="wrap">
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                     <input class="mdl-textfield__input" type="text" value="" name="notes" id="searchKey">
+                     <label class="mdl-textfield__label" for="searchKey">搜索……</label>
+                </div>
+                <!-- 查询按钮 -->
+                <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" id='searchBtn'>查询</button>
+                <!-- 绘制表格 -->
+                <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp" border=1px id='searchShow'></table>
+                </div>
+                <br>
+            </center>
         </div>
-        <br>
-        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-             <input class="mdl-textfield__input" type="text" name="bookName" required="required" id="bookName">
-             <label class="mdl-textfield__label" for="bookName">电影名称(必填)</label>
-        </div>
-        <br>
-        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-             <input class="mdl-textfield__input" type="text" name="link" pattern="[A-Z,a-z,0-9]*" id="link">
-             <label class="mdl-textfield__label" for="link">资源链接(必须是永久链接)</label>
-             <span class="mdl-textfield__error">链接只能包含字母和数字</span>
-        </div>
-        <br>
-        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-             <input class="mdl-textfield__input" type="text" name="extractCode" id="extractCode">
-             <label class="mdl-textfield__label" for="extractCode">提取码（如果有的话）</label>
-        </div>
-        <br>
-        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-             <textarea class="mdl-textfield__input" type="text" name="notes" id="notes" ></textarea>
-             <label class="mdl-textfield__label" for="notes">备注……</label>
-        </div>
-        <br>
-        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored" type="submit">
-            分享资源
-        </button>
-     </form>
-     <br>
-     <!-- <div class="mdl-spinner mdl-js-spinner is-active"></div> -->
-     <div id="load"><span class="mdl-chip"><span class="mdl-chip__text">加载中……</span></span></div>
-     <br>
-
-    <div class="wrap">
-    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-         <input class="mdl-textfield__input" type="text" value="" name="notes" id="searchKey">
-         <label class="mdl-textfield__label" for="searchKey">搜索……</label>
+      </main>
     </div>
-    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" id='searchBtn'>查询</button>
-    <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp" border=1px id='searchShow'></table>
-    </div>
-    <br>
-    </center>
 </body>
-
 <!-- Google mdl风格 -->
-<!-- <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"> -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="mdl/material.teal-light_blue.min.css" />
 <script defer src="mdl/material.min.js"></script>
 
@@ -165,6 +193,7 @@ Fuzzysearch.prototype={
     }
 }
 document.getElementById('load').innerHTML="<span class=\"mdl-chip\"><span class=\"mdl-chip__text\">加载成功！</span></span>";
+
 new Fuzzysearch(l);
 </script>
 </html>
