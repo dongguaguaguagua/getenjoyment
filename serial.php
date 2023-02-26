1b83405cb068d7b257597f4059fd3515688ac139
胡宗尧 <2022181640245@stu.scu.edu.cn>
2023-02-26 18:12:00 +0800

    add useless search button

--- serial.php
+++ serial.php
@@ -32,6 +32,16 @@ $result=init("localhost","root","","resources","serial","serialName");
                     <a class="mdl-navigation__link" href="/resources/variety_show.php">综艺</a>
                     <a class="mdl-navigation__link" href="/resources/books.php">书籍</a>
                 </nav>
+                <!-- 没有用的search button等你完善-->
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
+                                  mdl-textfield--floating-label mdl-textfield--align-right">
+                    <label class="mdl-button mdl-js-button mdl-button--icon" for="fixed-header-drawer-exp">
+                        <i class="material-icons">search</i>
+                    </label>
+                    <div class="mdl-textfield__expandable-holder">
+                        <input class="mdl-textfield__input" type="text" name="sample" id="fixed-header-drawer-exp">
+                    </div>
+                </div>
             </div>
         </header>
         <div class="mdl-layout__drawer">
