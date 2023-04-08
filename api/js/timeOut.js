e755326fcbf70ca4e11c08e21b2cdd574adad9fa
dongguaguaguagua <1448424184@qq.com>
2023-04-08 12:26:08 +0800

    debuging

--- api/js/timeOut.js
+++
@@ -1,7 +0,0 @@
-// 显示1秒的加载进度条
-setTimeout(function() {
-    // Get a reference to the loading spinner element
-    var spinner = document.getElementById('id_load');
-    // Remove the loading spinner from the DOM
-    spinner.parentNode.removeChild(spinner);
-}, 1000);
